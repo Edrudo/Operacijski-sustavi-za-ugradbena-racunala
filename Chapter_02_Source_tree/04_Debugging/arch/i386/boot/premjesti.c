5aca3c3f443cb3286f42d08f89939d05497b0493
eduardduras <eduard.duras@axilis.com>
2023-04-06 17:01:40 +0200

    v2

--- Chapter_02_Source_tree/04_Debugging/arch/i386/boot/premjesti.c
+++ Chapter_02_Source_tree/04_Debugging/arch/i386/boot/premjesti.c
@@ -2,22 +2,38 @@
 void premjesti()
 {
 	extern char size_data;
+	extern size_t size_c;
 	extern size_t size_i;
-	size_t size_d = (size_t) &size_data;
+	extern size_t size_d;
+	extern size_t size_b;
+	size_t size_code = (size_t) &size_c;
+	size_t size_instruction = (size_t) &size_i;
+	size_t size_constants = (size_t) &size_d;
+	size_t size_data = (size_t) &size_b;
 	size_t i;
-	char *od = (char *) 0x50000;
+
+	// code
+	char *od = (char *) 0x100000;
+	char *kamo = (char *) 0x0;
+	for ( i = 0; i< size_d; i++ )
+		*kamo++ = *od++;
+		
+	// instructions
+	char *od = (char *) 0x100000 + size_code;
+	char *kamo = (char *) 0x200000;
+	for ( i = 0; i< size_d; i++ )
+		*kamo++ = *od++;
+
+	// constants
+	char *od = (char *) 0x100000 + size_code + size_instruction;
+	char *kamo = (char *) 0x300000;
+	for ( i = 0; i< size_d; i++ )
+		*kamo++ = *od++;
+		
+	// data
+	char *od = (char *) 0x100000 + size_code + size_instruction + size_constants;
 	char *kamo = (char *) 0x400000;
-
-	//for ( i = 0; i< size_d; i++ )
-	//	kamo[i] = od[i];
-
 	for ( i = 0; i< size_d; i++ )
 		*kamo++ = *od++;
 
-	od = (char *) 0x10000;
-	kamo = (char *) 0x200000;
-	size_d = (size_t) &size_i;
-
-	for ( i = 0; i< size_d; i++ )
-		*kamo++ = *od++;
 }
