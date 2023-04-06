8f91e7482b07142630776f3d446700cd990b6dee
eduardduras <eduard.duras@axilis.com>
2023-04-06 18:46:54 +0200

    sds

--- Chapter_02_Source_tree/04_Debugging/arch/i386/boot/premjesti.c
+++ Chapter_02_Source_tree/04_Debugging/arch/i386/boot/premjesti.c
@@ -11,27 +11,22 @@ void premjesti()
 	size_t size_data = (size_t) &size_b;
 	size_t i;
 
-	// code
-	char *od = (char *) 0x100000;
-	char *kamo = (char *) 0x100000;
-	for ( i = 0; i< size_code; i++ )
-		*kamo++ = *od++;
 		
 	// instructions
-	od = (char *) 0x100000 + size_code;
-	kamo = (char *) 0x200000;
+	char *od = (char *) (0x100000 + size_code);
+	char *kamo = (char *)(0x200000);
 	for ( i = 0; i< size_instruction; i++ )
 		*kamo++ = *od++;
 
 	// constants
-	od = (char *) 0x100000 + size_code + size_instruction;
-	kamo = (char *) 0x300000;
+	od = (char *) (0x100000 + size_code + size_instruction);
+	kamo = (char *) (0x300000);
 	for ( i = 0; i< size_constants; i++ )
 		*kamo++ = *od++;
 		
 	// data
-	od = (char *) 0x100000 + size_code + size_instruction + size_constants;
-	kamo = (char *) 0x400000;
+	od = (char *) (0x100000 + size_code + size_instruction + size_constants);
+	kamo = (char *) (0x400000);
 	for ( i = 0; i< size_data; i++ )
 		*kamo++ = *od++;
 
