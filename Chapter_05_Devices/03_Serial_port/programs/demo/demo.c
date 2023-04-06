7593e827ab99a90814dadd951692d4bce1eace14
eduardduras <eduard.duras@axilis.com>
2023-04-06 18:10:24 +0200

    sds

--- Chapter_05_Devices/03_Serial_port/programs/demo/demo.c
+++ Chapter_05_Devices/03_Serial_port/programs/demo/demo.c
@@ -8,46 +8,29 @@
 
 int demo()
 {
-	void *ptr1, *ptr2, *ptr3, *ptr4, *ptr5;
+	void *ptrs[15];
 
-	ptr1 = malloc(10);
-	free(ptr1);
-	printf("\n\n\n\n\n");
+    for(int i = 0; i < 15; i++){
+        ptrs[i] = malloc(100);
+    }
 
-	ptr1 = malloc(1800000);
-	printf("adresa od ptr1 je %d\n", ptr1);
-	ptr2 = malloc(1800000);
-	printf("adresa od ptr2 je %d\n", ptr2);
-	ptr3 = malloc(1800000);
-	printf("adresa od ptr3 je %d\n", ptr3);
-	ptr4 = malloc(1800000);
-	printf("adresa od ptr4 je %d\n", ptr4);
+    printf("----------------\n");
+    
+    for(int i = 0; i < 4; i++){
+        free(ptrs[i]);
+    }
 
-	// try to allocate more memory than you have
-	ptr5 = malloc(300000);
-	if(ptr5){
-		printf("adresa od ptr5 je %d\n", ptr5);	
-		free(ptr5);
-	} else {
-		printf("nema dovoljno memorije za ptr5\n");
-	}
-	if(ptr2){
-		free(ptr2);
-	}
-	if (ptr3)
-	{
-		free(ptr3);
-	}
-	// try to allocate more memory than you have
-	ptr5 = malloc(3000000);
-	if(ptr5){
-		printf("adresa od ptr5 je %d\n", ptr5);	
-	} else {
-		printf("nema dovoljno memorije za ptr5\n");
-	}
+    printf("----------------\n");
+     
+    ptrs[0] = malloc(300);
 
-
-
+    printf("----------------\n");
+    
+    for(int i = 5; i < 15; i++){
+        free(ptrs[i]);
+    }
+    
+    printf("----------------\n");
 
 #if 0	/* test escape sequence */
 	printf("\x1b[20;40H" "Hello World at 40, 20!\n");
