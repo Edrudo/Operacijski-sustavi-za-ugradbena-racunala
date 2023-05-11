9cbc8b9352e192a1b3ca24f8bb5526b836888733
eduardduras <eduard.duras@axilis.com>
2023-05-11 10:04:02 +0200

    lab4x v1

--- Chapter_05_Devices/03_Serial_port/programs/timer/timer.c
+++ Chapter_05_Devices/03_Serial_port/programs/timer/timer.c
@@ -138,7 +138,7 @@ int timer ()
 	timer_create ( CLOCK_MONOTONIC, &evp3, &timer3 );
 	timer_settime ( &timer3, 0, &t2, NULL );
 
-	while (TIME_IS_SET(&t))
+	while (1)
 		if (clock_nanosleep(CLOCK_REALTIME, 0, &t, &t))
 			printf("Interrupted sleep?\n");
 
